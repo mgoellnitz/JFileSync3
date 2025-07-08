dbeb5ab7bae7439d26c1b0834c2410a1ed425c6c
Martin Goellnitz <martin@goellnitz.de>
2025-07-08 10:45:01 +0200

    Fix relocation script

--- xdg/prepare_desktop_file.sh
+++ xdg/prepare_desktop_file.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 DIR=$(dirname $(readlink -f $0))
 if [ -f $DIR/../lib/JFileSync3.jar ] ; then
-  PATTERN=$(find $DIR -name "JFileSync3.png"|sed -e 's/\//\\\//g')\\/
+  PATTERN=$(echo $DIR|sed -e 's/\//\\\//g')\\/
+  IMAGE_FILE=$(find $DIR -name "JFileSync3.png"|sed -e 's/\//\\\//g')
   sed -i.bak -e "s/^Exec=.*/Exec=${PATTERN}JFileSync3/" $DIR/JFileSync3.desktop
-  sed -i.bak -e "s/^Icon=.*/Icon=${PATTERN}JFileSync3.png/" $DIR/JFileSync3.desktop
+  sed -i.bak -e "s/^Icon=.*/Icon=${IMAGE_FILE}/" $DIR/JFileSync3.desktop
   rm $DIR/JFileSync3.desktop.bak
 fi
