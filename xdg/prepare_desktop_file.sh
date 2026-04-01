6fc0390c82e2d6f973d539d57d7029412a34e4f8
Martin Goellnitz <martin@goellnitz.de>
2026-04-01 16:23:01 +0200

    More flexible desktop file preparation

--- xdg/prepare_desktop_file.sh
+++ xdg/prepare_desktop_file.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
+#
+# Copyright 2024-2026 Martin Goellnitz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 DIR=$(dirname $(readlink -f $0))
 if [ -f $DIR/../lib/JFileSync3.jar ] ; then
+  LIBDIR=$(dirname $(readlink -f $DIR/../lib/JFileSync3.jar))
   PATTERN=$(echo $DIR|sed -e 's/\//\\\//g')\\/
-  IMAGE_FILE=$(find $DIR -name "JFileSync3.png"|sed -e 's/\//\\\//g')
+  IMAGE_FILE=$(find $LIBDIR -name "JFileSync3.png"|sed -e 's/\//\\\//g')
   sed -i.bak -e "s/^Exec=.*/Exec=${PATTERN}JFileSync3/" $DIR/JFileSync3.desktop
-  sed -i.bak -e "s/^Icon=.*/Icon=${IMAGE_FILE}/" $DIR/JFileSync3.desktop
-  rm $DIR/JFileSync3.desktop.bak
+  sed -i.bak -e "s/^Icon=.*/Icon=$IMAGE_FILE/" $DIR/JFileSync3.desktop
+  rm -f $DIR/JFileSync3.desktop.bak
 fi
